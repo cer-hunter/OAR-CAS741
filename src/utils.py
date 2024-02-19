e2ae99599a4b320928ed8b89362b2d59cf701e1f
Hunter Ceranic <ceranic.ind@gmail.com>
2024-02-19 14:41:41 -0500

    update srs

--- src/utils.py
+++ src/utils.py
@@ -1,11 +1,13 @@
-#This file contains all the theoretical model calculations for the OAR Project
+# This file contains all the theoretical model calculations for the OAR Project
 import numpy as np
 
-#Sigmoid Function, corresponding to TM1
+
+# Sigmoid Function, corresponding to TM1
 def sigmoid(z):
     return (1 / 1 + np.exp(-z))
 
-#Log Loss Function, corresponding to TM2
+
+# Log Loss Function, corresponding to TM2
 def logLossFunction(y_true, y_hat):
     y_true = np.array(y_true)
     y_hat = np.array(y_hat)
@@ -16,19 +18,31 @@ def logLossFunction(y_true, y_hat):
         log_loss = log_loss + (-1/n)*cost
     return log_loss
 
-#computes the sigmoid function for a predicted value, corresponding to GD1
+
+# computes the sigmoid function for a predicted value, corresponding to GD1
 def y_prediction(x, w, b):
     y_hat = sigmoid(np.dot(np.transpose(w), x) + b)
     return y_hat
 
-#computes the gradient with respect to the weights, corresponding to GD2
+
+# computes the gradient with respect to the weights, corresponding to GD2
 def gradient_w(x, y, w, b, reg_lambda, N):
     y_hat = y_prediction(x, w, b)
     dw = x * (y-y_hat) - (reg_lambda*w*w)/N
     return dw
 
-#computes the gradient with respect to the bias, corresponding to GD3
+
+# computes the gradient with respect to the bias, corresponding to GD3
 def gradient_b(x, y, w, b):
     y_hat = y_prediction(x, w, b)
     db = y - y_hat
     return db
+
+
+# computes the gradient descent for one epoch, corresponding to IM1
+def gradient_descent(x, y, w, b, reg_lambda, alpha, N):
+    dw = gradient_w(x, y, w, b, reg_lambda, N)
+    db = gradient_w(x, y, w, b)
+    w = w + alpha*dw
+    b = b + alpha*db
+    return w, b
