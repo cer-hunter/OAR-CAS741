bc79b8e5e73a731e998363811c3bf7c258d7cfa1
Hunter Ceranic <ceranic.ind@gmail.com>
2024-04-03 12:42:18 -0400

    code for input/preprocess modules

--- src/input.py
+++ src/input.py
@@ -1,5 +1,28 @@
-import numpy
-import sklearn
 import cv2
-import seaborn
-import matplotlib
+import os
+from preprocess import preprocess
+
+
+# Prompt for input done by GUI, this verifies and processes image
+def input(imgPath):
+
+    # check file is correct and contains an image of .BMP, .JPG, .PNG
+    if (not os.path.isfile(imgPath)):
+        raise Exception("This is not a proper image file")
+    ext = os.path.splitext(imgPath)[-1].lower()
+    if (ext != '.png' and ext != '.jpg' and ext != '.bmp'):
+        raise TypeError("This is not a JPG, BMP or PNG image file")
+
+    baseImg = cv2.imread(imgPath)
+
+    # first check size/shape is within bounds
+    height = baseImg.shape[0]
+    width = baseImg.shape[1]
+    if ((height > 4096) or (width > 4096)):
+        raise ValueError("This image is too large")
+    elif ((height < 20) or (width < 20)):
+        raise ValueError("This image is too small")
+    else:
+        # now process image
+        dispImg, inputImg = preprocess(baseImg)
+    return dispImg, inputImg
--- src/preprocess.py
+++ src/preprocess.py
@@ -1,5 +1,25 @@
-import numpy
-import sklearn
 import cv2
-import seaborn
-import matplotlib
+
+
+PX_SIZE = 28  # since the input images in the data set used to train were 28x28
+PX_VAL = 255  # Max pixel value for grayscale image
+
+
+def preprocess(baseImg):
+    # resize the image
+    dispResize = cv2.resize(baseImg, (200, 200))  # Larger for display to GUI
+    inputResize = cv2.resize(baseImg, (PX_SIZE, PX_SIZE))  # For classification
+
+    # Rearrange colour channels for displaying on GUI
+    b, g, r = cv2.split(dispResize)
+    dispImg = cv2.merge((r, g, b))
+
+    # Now for processing the image to be classified...
+    # First convert resized image to grayscale
+    grayImage = cv2.cvtColor(inputResize, cv2.COLOR_BGR2GRAY)
+    # Flatten Image into one array
+    inputImg = grayImage.flatten()
+    # Normalize values
+    inputImg = inputImg.astype('float32')/PX_VAL
+
+    return dispImg, inputImg
