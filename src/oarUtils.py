f4ddc92392761ddf106aff8b6ab0e7a6ed0bed51
Hunter Ceranic <ceranic.ind@gmail.com>
2024-04-03 12:40:10 -0400

    oarUtils Module
    contains fundamental math equations

--- src/oarUtils.py
+++ src/oarUtils.py
@@ -1,35 +1,52 @@
 # This file contains all the theoretical model calculations for the OAR Project
 import numpy as np
 
+# Depending on the model used to classify images you might have constraints
+# on how many predictors you can use before over-fitting begins to occur...
+# In our case Logistic Regression is often suggested to use 10 predictors
+# We use 782 pixels... as such scaling by a factor of 10^2 is neccessary
+SCALE = 100
+
 
 # Sigmoid Function, corresponding to TM1
 def sigmoid(z):
-    return (1 / 1 + np.exp(-z))
+    return (1/(1 + np.exp(-z)))
 
 
 # Log Loss Function, corresponding to TM2
+# L = - (yTrue*log(yHat) + (1-yTrue)*(log(1-yHat))
+# Since yTrue is binary, find the value of 1-yTrue first
+# To avoid any potential divide by zero errors
 def logLossFunc(yTrue, yHat):
     logLoss = 0
-    cost = yTrue*np.log10(yHat) + (1-yTrue)*np.log10(1-yHat)
-    logLoss = logLoss + (-1)*cost
+    if ((1 - yTrue) == 0):
+        # yTrue corresponds to a label
+        logLoss = np.multiply(yTrue, np.log(yHat))
+    else:
+        # yTrue does not correspond to a label
+        logLoss = np.multiply((1-yTrue), np.log(abs(1-yHat)))
+    logLoss -= logLoss
     return logLoss
 
 
 # computes the sigmoid function for a predicted value, corresponding to GD1
+# This is the probability a prediction is correct
 def predictSigmoid(x, w, b):
-    yHat = sigmoid(np.dot(np.transpose(w), x) + b)
+    z = np.dot(np.transpose(w), x) + b
+    z = z/SCALE
+    yHat = sigmoid(z)
     return yHat
 
 
 # computes the gradient with respect to the weights, corresponding to GD2
 def gradientW(x, y, w, b, regLambda, N):
     yHat = predictSigmoid(x, w, b)
-    dW = np.transpose(x) * (yHat - y) - (regLambda*w*w)/N
+    dW = x*(y-yHat)-((regLambda*w*w)/N)
     return dW
 
 
 # computes the gradient with respect to the bias, corresponding to GD3
 def gradientB(x, y, w, b):
     yHat = predictSigmoid(x, w, b)
-    dB = yHat - y
+    dB = y-yHat
     return dB
