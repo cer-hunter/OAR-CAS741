cc6ee4887cf7f3db32843aea4fff5eeda8c8c83f
Hunter Ceranic <ceranic.ind@gmail.com>
2024-04-14 17:42:09 -0400

    please work tests

--- tests/test_req.py
+++ tests/test_req.py
@@ -1,35 +1,33 @@
 import sys
 import pytest
 import numpy as np
-from pathlib import Path
-from os.path import join
+from os.path import join, dirname
 sys.path.insert(0, "../src/")
 from src.input import input
 
-ROOT_DIR = Path(__file__).parent.parent
-PATH = join(ROOT_DIR, "/tests/test-images/")
+PATH = join(dirname(__file__), "test-images")
 
 INS = [join(PATH, "A.png"), join(PATH, "A.jpg"), join(PATH, "A.bmp")]
-ERR_IN = [PATH+"A.pdf", PATH+"Empty", None]
-SIZES = [PATH+"toobig.png", PATH+"big.png",
-         PATH+"small.png", PATH+"toosmall.png"]
-COLORS = [PATH+"A.png", PATH+"A_Blue.png", PATH+"A_Green.png",
-          PATH+"A_Red.png", PATH+"A_Gray.png"]
-LETTERS = [PATH+"A.jpg", PATH+"B.jpg", PATH+"C.jpg", PATH+"D.jpg",
-           PATH+"E.jpg", PATH+"F.jpg", PATH+"G.jpg", PATH+"H.jpg",
-           PATH+"I.jpg", PATH+"J.jpg", PATH+"K.jpg", PATH+"L.jpg",
-           PATH+"M.jpg", PATH+"N.jpg", PATH+"O.jpg", PATH+"P.jpg",
-           PATH+"Q.jpg", PATH+"R.jpg", PATH+"S.jpg", PATH+"T.jpg",
-           PATH+"U.jpg", PATH+"V.jpg", PATH+"W.jpg", PATH+"X.jpg",
-           PATH+"Y.jpg", PATH+"Z.jpg"]
-ANGLES = [PATH+"angle0.png", PATH+"angle1p.png", PATH+"angle1n.png",
-          PATH+"angle2p.png", PATH+"angle2n.png",
-          PATH+"angle5p.png", PATH+"angle5n.png",
-          PATH+"angle15p.png", PATH+"angle15n.png",
-          PATH+"angle45p.png", PATH+"angle45n.png",
-          PATH+"angle90p.png", PATH+"angle90n.png",
-          PATH+"angle135p.png", PATH+"angle135n.png", PATH+"angle180.png"]
-ERR_OUT = [PATH+"ABC.jpg", PATH+"hunter_ok.jpg", PATH+"Blank.jpg"]
+# ERR_IN = [PATH+"A.pdf", PATH+"Empty", None]
+# SIZES = [PATH+"toobig.png", PATH+"big.png",
+#          PATH+"small.png", PATH+"toosmall.png"]
+# COLORS = [PATH+"A.png", PATH+"A_Blue.png", PATH+"A_Green.png",
+#           PATH+"A_Red.png", PATH+"A_Gray.png"]
+# LETTERS = [PATH+"A.jpg", PATH+"B.jpg", PATH+"C.jpg", PATH+"D.jpg",
+#            PATH+"E.jpg", PATH+"F.jpg", PATH+"G.jpg", PATH+"H.jpg",
+#            PATH+"I.jpg", PATH+"J.jpg", PATH+"K.jpg", PATH+"L.jpg",
+#            PATH+"M.jpg", PATH+"N.jpg", PATH+"O.jpg", PATH+"P.jpg",
+#            PATH+"Q.jpg", PATH+"R.jpg", PATH+"S.jpg", PATH+"T.jpg",
+#            PATH+"U.jpg", PATH+"V.jpg", PATH+"W.jpg", PATH+"X.jpg",
+#            PATH+"Y.jpg", PATH+"Z.jpg"]
+# ANGLES = [PATH+"angle0.png", PATH+"angle1p.png", PATH+"angle1n.png",
+#           PATH+"angle2p.png", PATH+"angle2n.png",
+#           PATH+"angle5p.png", PATH+"angle5n.png",
+#           PATH+"angle15p.png", PATH+"angle15n.png",
+#           PATH+"angle45p.png", PATH+"angle45n.png",
+#           PATH+"angle90p.png", PATH+"angle90n.png",
+#           PATH+"angle135p.png", PATH+"angle135n.png", PATH+"angle180.png"]
+# ERR_OUT = [PATH+"ABC.jpg", PATH+"hunter_ok.jpg", PATH+"Blank.jpg"]
 
 
 # System Tests
