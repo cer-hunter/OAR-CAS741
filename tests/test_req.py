b97f3d4abd509a2cf09806acd6361529081c2d35
Hunter Ceranic <ceranic.ind@gmail.com>
2024-04-14 17:57:05 -0400

    try new input tests

--- tests/test_req.py
+++ tests/test_req.py
@@ -8,26 +8,32 @@ from src.input import input
 PATH = join(dirname(__file__), "test-images")
 
 INS = [join(PATH, "A.png"), join(PATH, "A.jpg"), join(PATH, "A.bmp")]
-# ERR_IN = [PATH+"A.pdf", PATH+"Empty", None]
-# SIZES = [PATH+"toobig.png", PATH+"big.png",
-#          PATH+"small.png", PATH+"toosmall.png"]
-# COLORS = [PATH+"A.png", PATH+"A_Blue.png", PATH+"A_Green.png",
-#           PATH+"A_Red.png", PATH+"A_Gray.png"]
-# LETTERS = [PATH+"A.jpg", PATH+"B.jpg", PATH+"C.jpg", PATH+"D.jpg",
-#            PATH+"E.jpg", PATH+"F.jpg", PATH+"G.jpg", PATH+"H.jpg",
-#            PATH+"I.jpg", PATH+"J.jpg", PATH+"K.jpg", PATH+"L.jpg",
-#            PATH+"M.jpg", PATH+"N.jpg", PATH+"O.jpg", PATH+"P.jpg",
-#            PATH+"Q.jpg", PATH+"R.jpg", PATH+"S.jpg", PATH+"T.jpg",
-#            PATH+"U.jpg", PATH+"V.jpg", PATH+"W.jpg", PATH+"X.jpg",
-#            PATH+"Y.jpg", PATH+"Z.jpg"]
-# ANGLES = [PATH+"angle0.png", PATH+"angle1p.png", PATH+"angle1n.png",
-#           PATH+"angle2p.png", PATH+"angle2n.png",
-#           PATH+"angle5p.png", PATH+"angle5n.png",
-#           PATH+"angle15p.png", PATH+"angle15n.png",
-#           PATH+"angle45p.png", PATH+"angle45n.png",
-#           PATH+"angle90p.png", PATH+"angle90n.png",
-#           PATH+"angle135p.png", PATH+"angle135n.png", PATH+"angle180.png"]
-# ERR_OUT = [PATH+"ABC.jpg", PATH+"hunter_ok.jpg", PATH+"Blank.jpg"]
+ERR_IN = [join(PATH, "A.pdf"), join(PATH, "Empty"), None]
+SIZES = [join(PATH, "toobig.png"), join(PATH, "big.png"),
+         join(PATH, "small.png"), join(PATH, "toosmall.png")]
+COLORS = [join(PATH, "A.png"), join(PATH, "A_Blue.png"),
+          join(PATH, "A_Green.png"), join(PATH, "A_Red.png"),
+          join(PATH, "A_Gray.png")]
+LETTERS = [join(PATH, "A.jpg"), join(PATH, "B.jpg"), join(PATH, "C.jpg"),
+           join(PATH, "D.jpg"), join(PATH, "E.jpg"), join(PATH, "F.jpg"),
+           join(PATH, "G.jpg"), join(PATH, "H.jpg"), join(PATH, "I.jpg"),
+           join(PATH, "J.jpg"), join(PATH, "K.jpg"), join(PATH, "L.jpg"),
+           join(PATH, "M.jpg"), join(PATH, "N.jpg"), join(PATH, "O.jpg"),
+           join(PATH, "P.jpg"), join(PATH, "Q.jpg"), join(PATH, "R.jpg"),
+           join(PATH, "S.jpg"), join(PATH, "T.jpg"), join(PATH, "U.jpg"),
+           join(PATH, "V.jpg"), join(PATH, "W.jpg"), join(PATH, "X.jpg"),
+           join(PATH, "Y.jpg"), join(PATH, "Z.jpg")]
+ANGLES = [join(PATH, "angle0.png"),
+          join(PATH, "angle1p.png"), join(PATH, "angle1n.png"),
+          join(PATH, "angle2p.png"), join(PATH, "angle2n.png"),
+          join(PATH, "angle5p.png"), join(PATH, "angle5n.png"),
+          join(PATH, "angle15p.png"), join(PATH, "angle15n.png"),
+          join(PATH, "angle45p.png"), join(PATH, "angle45n.png"),
+          join(PATH, "angle90p.png"), join(PATH, "angle90n.png"),
+          join(PATH, "angle135p.png"), join(PATH, "angle135n.png"),
+          join(PATH, "angle180.png")]
+ERR_OUT = [join(PATH, "ABC.jpg"), join(PATH, "hunter_ok.jpg"),
+           join(PATH, "Blank.jpg")]
 
 
 # System Tests
@@ -35,16 +41,18 @@ def returnInput(path):
     return input(path)
 
 
-@pytest.mark.set1
+@pytest.mark.vnv
 def test_input_format():
     for i in range(len(INS)):
         result1, result2 = returnInput(INS[i])
         assert isinstance(result1, np.ndarray)
         assert isinstance(result2, np.ndarray)
 
-
-# def test_input_exception():
-#     for i in ERR_IN:
-#         pytest.raises(ValueError, input, ERR_IN[i])
+@pytest.mark.vnv
+def test_invalid_input_format():
+    for i in range(len(ERR_IN)):
+        result1, result2 = returnInput(INS[i])
+        assert isinstance(result1, ValueError)
+        assert isinstance(result2, ValueError)
 
 # Unit Tests
